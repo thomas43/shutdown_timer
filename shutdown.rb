5f2bc631c43a8b29e16cb4da5d2d81d96611e98e
Thomas Greenman <tgreenman@mail.csuchico.edu>
2016-03-27 09:04:39 -0700

    initial commit of shutoff timer

---
+++ shutdown.rb
@@ -0,0 +1,22 @@
+#!/home/thomas/.shoes/walkabout/shoes
+Shoes.app(title: "Shutdown Timer", width: 400, height: 400, resizable: false) {
+  oval(left:   10,
+       top:    10,
+       radius: 40)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#######!/usr/bin/env ruby
