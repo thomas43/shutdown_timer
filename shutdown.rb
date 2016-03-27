9042dde1806771d688207357af4c8a9cb9e0b15d
Thomas Greenman <tgreenman@mail.csuchico.edu>
2016-03-27 10:27:00 -0700

    timer working

--- shutdown.rb
+++ shutdown.rb
@@ -5,10 +5,16 @@ Shoes.app(title: "Shutdown Timer", width: 400, height: 400, resizable: false) {
     para "Enter time value in minutes"
     @timeval = edit_line
     @push = button "Submit"
-    @time = para ""
+    @counter_text = para ""
     @push.click do
-      @time.replace "Computer will shut down in " + @timeval.text + " minute(s)."
+      @counter_text.replace "Computer will shut down in " + @timeval.text + " minute(s)."
+      # Every minute change printing value to reflect minutes left
+      every(5) do |count|
+        print_val = @timeval.text.to_i - count - 1
+        @counter_text.replace "Computer will shut down in " + print_val.to_s + " minute(s)."
+      end
     end
+
   end
 }
 
